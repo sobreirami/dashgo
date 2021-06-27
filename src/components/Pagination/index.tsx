40fef35969615a2450c9460cc297bab599acd0a8
Michel Rodrigues <sobreira.michel@gmail.com>
2021-06-27 11:30:19 -0300

    logic pagination

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,7 +1,38 @@
-import { Stack, Button, Box } from "@chakra-ui/react";
+import { Stack, Button, Box, Text } from "@chakra-ui/react";
 import { PaginationItem } from "./PaginationItem";
 
-export function Pagination() {
+interface PaginationProps {
+    totalCountOfRegisters: number;
+    registersPerPage?: number;
+    currentPage?: number;
+    onPageChange: (page: number) => void;
+}
+
+const siblingsCount = 1;
+
+function generatePagesArray(from: number, to: number) {
+    return [...new Array(to - from)].map((_, index) => {
+        return from + index + 1;
+    })
+    .filter(page => page > 0)
+}
+
+export function Pagination({ 
+    totalCountOfRegisters,
+    currentPage = 1,
+    onPageChange,
+    registersPerPage = 10
+ }: PaginationProps) {
+    const lastPage = Math.floor(totalCountOfRegisters / registersPerPage);
+
+    const previousPages = currentPage > 1
+        ? generatePagesArray(currentPage - 1 - siblingsCount, currentPage - 1)
+        : [];
+
+    const nextPages = currentPage < lastPage
+        ? generatePagesArray(currentPage, Math.min(currentPage + siblingsCount, lastPage))
+        : [];
+
     return (
         <Stack
             direction={["column","row"]}
@@ -17,10 +48,33 @@ export function Pagination() {
                 direction="row"
                 spacing="2"
             >
-                <PaginationItem number={1} isCurrent />
-                <PaginationItem number={2} />
-                <PaginationItem number={3} />
-                <PaginationItem number={4} />
+                { currentPage > ( 1 + siblingsCount ) && (
+                    <>
+                        <PaginationItem onPageChange={onPageChange} number={1} />
+                        { currentPage > ( 2 + siblingsCount ) && (
+                            <Text color="green.300" width="8"textAlign="center">...</Text>
+                        ) }
+                    </>
+                ) }
+
+                {previousPages.length > 0 && previousPages.map(page => (
+                    <PaginationItem onPageChange={onPageChange} key={page} number={page} />
+                ))}
+
+                <PaginationItem onPageChange={onPageChange} number={currentPage} isCurrent />
+
+                {nextPages.length > 0 && nextPages.map(page => (
+                    <PaginationItem onPageChange={onPageChange} key={page} number={page} />
+                ))}
+
+                { (currentPage + siblingsCount) < lastPage && (
+                    <>
+                        { (currentPage + 1 + siblingsCount) < lastPage && (
+                            <Text color="green.300" width="8"textAlign="center">...</Text>
+                        ) }
+                        <PaginationItem onPageChange={onPageChange} number={lastPage} /> 
+                    </>
+                ) }
             </Stack>
         </Stack>
     );
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Link from 'next/link';
 import { 
     Box, 
@@ -20,32 +21,14 @@ import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
-import { useEffect } from 'react';
 
 import { useQuery } from 'react-query';
+import { api } from '../../services/api';
+import { useUsers } from '../../services/hooks/useUsers';
 
 export default function Userlist() {
-    const { data, isLoading, error } = useQuery('users', async () => {
-        const response = await fetch('http://localhost:3000/api/users')
-        const data = await response.json();
-        
-        const users = data.users.map(user => {
-            return {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-                    day: '2-digit',
-                    month: 'long',
-                    year: 'numeric'
-                }),
-            };
-        });
-
-        return users;
-    }, {
-        staleTime: 1000 * 5
-    });
+    const [page, setPage] = useState(1);
+    const { data, isLoading, error, isFetched } = useUsers(page);
 
     const isWideVersion = useBreakpointValue({
         base: false,
@@ -68,7 +51,10 @@ export default function Userlist() {
 
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+                        <Heading size="lg" fontWeight="normal">
+                            Usuários
+                            { !isLoading && isFetched && <Spinner size="sm" color="gray.500" ml="4" /> }
+                        </Heading>
 
                         <Link href="/users/create" passHref>
                             <Button 
@@ -113,7 +99,7 @@ export default function Userlist() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    { data.map((user => (
+                                    { data.users.map((user => (
                                         <Tr key={user.id}>
                                             <Td px={["4","4","6"]}>
                                                 <Checkbox colorScheme="pink" />
@@ -149,7 +135,11 @@ export default function Userlist() {
                                 </Tbody>
                             </Table>
 
-                            <Pagination />
+                            <Pagination
+                                totalCountOfRegisters={data.totalCount}
+                                currentPage={page}
+                                onPageChange={setPage}
+                            />
                         </>
                     ) }
                 </Box>
